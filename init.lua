cb24aed962a04314d11596f9c08853e6b062544e
hampgoodwin <hampgoodwin@gmail.com>
2023-11-17 13:06:57 -0700

    add hidden file for live_grep; dep upds

--- init.lua
+++ init.lua
@@ -329,6 +329,21 @@ require('telescope').setup {
       "ios/Pods",
       "vendor",
     },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden'
+    },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
   },
 }
 

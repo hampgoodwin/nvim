b19494da22cce4a4529e380050b16db3be12e0c2
hampgoodwin <hampgoodwin@gmail.com>
2024-02-09 11:41:32 -0700

    remove vimhelm

--- init.lua
+++ init.lua
@@ -235,9 +235,6 @@ require('lazy').setup({
   {
     'windwp/nvim-ts-autotag',
   },
-  {
-    'towolf/vim-helm',
-  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.

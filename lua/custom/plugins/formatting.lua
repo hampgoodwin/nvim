2d02f24d7111b11f3956ffd2a00f01a36f120f40
hampgoodwin <hampgoodwin@gmail.com>
2024-03-12 10:41:34 -0600

    use funcs to enable/disable formatting

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -1,23 +1,44 @@
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		--FormatDisable will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
 return {
 	"stevearc/conform.nvim",
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+	-- cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			go = { "gofumpt" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+		},
+		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return {
 
-		conform.setup({
-			formatters_by_ft = {
-				go = { "gofumpt" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-			},
-			format_on_save = {
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 1000,
-			},
-		})
-	end
+			}
+		end
+	},
 }

cb34b6c5bb0588d1f9e6c373e7a7b48b2e9b70c1
hgoodwin <hgoodwin@pryoninc.com>
2024-04-05 11:07:28 -0600

    update treesitter parsers; upd deps

--- init.lua
+++ init.lua
@@ -372,7 +372,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'gomod', 'gosum', 'gowork', 'graphql', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-    'toml', 'json', 'html', 'sql', 'terraform', 'kdl', },
+    'toml', 'json', 'html', 'sql', 'terraform', 'kdl', 'proto' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,

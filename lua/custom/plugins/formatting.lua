f80ce024ffbbf9cec94e3df6f7d9db8719e1e6bd
hampgoodwin <hampgoodwin@gmail.com>
2024-07-12 23:00:53 -0600

    make similar to latest kickstart, various upd

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -20,25 +20,37 @@ return {
 	"stevearc/conform.nvim",
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" },
-	-- cmd = { "ConformInfo" },
+	keys = {
+		{
+			'<leader>f',
+        		function()
+				require('conform').format { async = true, lsp_fallback = true }
+        		end,
+        		mode = '',
+        		desc = '[F]ormat buffer',
+		},
+	},
 	opts = {
+		notify_on_error = false,
 		formatters_by_ft = {
 			go = { "gofumpt" },
 			javascript = { "prettierd" },
 			javascriptreact = { "prettierd" },
 			typescript = { "prettierd" },
 			typescriptreact = { "prettierd" },
+			nix = { "nixfmt" },
+			lua = { "stylua" },
 		},
 		format_on_save = function(bufnr)
 			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 				return
 			end
+			local disable_filetypes = { c = true, cpp = true }
 			return {
-
-				lsp_fallback = true,
+				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 				async = false,
 				timeout_ms = 1000,
 			}
-		end
+		end,
 	},
 }

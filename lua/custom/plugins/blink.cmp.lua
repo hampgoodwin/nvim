0a25c52cd09623e7d9498bc10f52de23ed1727a8
hampgoodwin <hampgoodwin@gmail.com>
2025-07-30 10:48:32 -0600

    win border and blink upd

--- lua/custom/plugins/blink.cmp.lua
+++ lua/custom/plugins/blink.cmp.lua
@@ -34,25 +34,32 @@ return {
         fuzzy = { implementation = 'prefer_rust_with_warning' },
         completion = {
           documentation = {
-            auto_show = true, -- to display documentation automatically, set to true
+            auto_show = false, -- to display documentation automatically, set to true
             auto_show_delay_ms = 500,
           },
           menu = {
             draw = {
               treesitter = { 'lsp' },
               align_to = 'kind_icon',
-              columns = {
-                { 'kind_icon' },
-                { 'source_name', gap = 1 },
-                { 'label', 'label_description', gap = 1 },
-              },
+              -- columns = {
+              --   { 'kind_icon' },
+              --   { 'source_name', gap = 1 },
+              --   { 'label', 'label_description', gap = 1 },
+              -- },
               components = {
                 kind_icon = {
-                  ellipsis = false,
                   text = function(ctx)
                     local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
                     return kind_icon
                   end,
+                  -- (optional) use highlights from mini.icons
+                  highlight = function(ctx)
+                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                    return hl
+                  end,
+                },
+                kind = {
+                  -- (optional) use highlights from mini.icons
                   highlight = function(ctx)
                     local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
                     return hl

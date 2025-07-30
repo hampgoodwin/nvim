6110f535472b3159d3f3f5f9c723f10ca8c00912
Hamp Goodwin <hampgoodwin@gmail.com>
2025-07-29 21:29:12 -0600

    update catppuccin plugin

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -6,16 +6,23 @@ return {
   opts = {
     flavour = 'auto',
     background = {
-      light = 'latte',
+      light = 'frappe',
       dark = 'macchiato',
     },
+    dim_inactive = {
+      enabled = false, -- dims the background color of inactive window
+      shade = 'dark',
+      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
     integrations = {
-      blink_cmp = true,
+      blink_cmp = {
+        style = 'bordered',
+      },
       dap = true,
       dap_ui = true,
       fzf = true,
       gitsigns = true,
-      mini = { enabled = true, indentscope_color = 'macchiato' },
+      mini = { enabled = true, indentscope_color = 'frappe' },
       native_lsp = {
         enabled = true,
         virtual_text = {
@@ -37,8 +44,7 @@ return {
         },
       },
       neotest = true,
-      snacks = { enabled = true, indent_scope_color = 'macchiato' },
-      telescope = { enabled = false },
+      snacks = { enabled = true, indent_scope_color = 'lavender' },
       treesitter = true,
       which_key = true,
     },
